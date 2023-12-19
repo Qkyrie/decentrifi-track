32ddb9c0506504c6a8426b0448e34c63e90e182f
Quinten De Swaef <quintendeswaef@gmail.com>
2023-12-19 09:14:11 +0100

    history with pagination

--- src/api/whalespotter/transactions/transactions.js
+++ src/api/whalespotter/transactions/transactions.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-export async function getEvents(address) {
-    const result = await axios.get(`https://whalespotter.decentri.fi/events/${address}`);
+export async function getEvents(address, page) {
+    const actualPage = (page === null ? 0 : page - 1)
+    const result = await axios.get(`https://whalespotter.decentri.fi/events/${address}?page=${actualPage}`);
     return await result.data;
 }
 
